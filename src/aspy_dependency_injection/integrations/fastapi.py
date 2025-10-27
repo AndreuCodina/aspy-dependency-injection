81f6d36309ec45d9dd79088388727833e0cdced4
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2025-10-27 07:06:14 +0100

    Add methods to FastApiDependencyInjection

--- src/aspy_dependency_injection/integrations/fastapi.py
+++ src/aspy_dependency_injection/integrations/fastapi.py
@@ -27,17 +27,108 @@ if TYPE_CHECKING:
 current_request: ContextVar[Request | WebSocket] = ContextVar("aspy_starlette_request")
 
 
-def _update_lifespan(app: FastAPI, service_provider: DefaultServiceProvider) -> None:
-    old_lifespan = app.router.lifespan_context
+class FastApiDependencyInjection:
+    def setup(self, app: FastAPI, services: ServiceCollection) -> None:
+        service_provider = services.build_service_provider()
+        app.state.aspy_service_provider = service_provider
+        app.add_middleware(_AspyAsgiMiddleware)
+        self._update_lifespan(app, service_provider)
+        self._inject_routes(app.routes)
 
-    @asynccontextmanager
-    async def new_lifespan(app: FastAPI) -> AsyncGenerator[Any]:
-        async with old_lifespan(app) as state:
-            yield state
+    def _update_lifespan(
+        self, app: FastAPI, service_provider: DefaultServiceProvider
+    ) -> None:
+        old_lifespan = app.router.lifespan_context
 
-        await service_provider.__aexit__(None, None, None)
+        @asynccontextmanager
+        async def new_lifespan(app: FastAPI) -> AsyncGenerator[Any]:
+            async with old_lifespan(app) as state:
+                yield state
 
-    app.router.lifespan_context = new_lifespan
+            await service_provider.__aexit__(None, None, None)
+
+        app.router.lifespan_context = new_lifespan
+
+    def _are_annotated_parameters_with_aspy_dependencies(
+        self,
+        target: Callable[..., Any],
+    ) -> bool:
+        for parameter in inspect.signature(target).parameters.values():
+            if parameter.annotation is not None and isinstance(
+                parameter.annotation, InjectableType
+            ):
+                return True
+
+        return False
+
+    def _inject_routes(self, routes: list[BaseRoute]) -> None:
+        for route in routes:
+            if not (
+                isinstance(route, APIRoute)
+                and route.dependant.call is not None
+                and not self._are_annotated_parameters_with_aspy_dependencies(
+                    route.dependant.call
+                )
+            ):
+                continue
+
+            route.dependant.call = self._inject_from_container(route.dependant.call)
+
+    def _inject_from_container(self, target: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.wraps(target)
+        async def _inject_async_target(*args: Any, **kwargs: Any) -> Any:  # noqa: ANN401
+            parameters_to_inject = self._get_parameters_to_inject(target)
+            parameters_to_inject_resolved: dict[str, Any] = {
+                injected_parameter_name: await self._get_request_container().get_service(
+                    injected_parameter_class
+                )
+                for injected_parameter_name, injected_parameter_class in parameters_to_inject.items()
+            }
+            return await target(*args, **{**kwargs, **parameters_to_inject_resolved})
+
+        return _inject_async_target
+
+    def _get_request_container(self) -> ServiceScope:
+        """When inside a request, returns the scoped container instance handling the current request.
+
+        This is what you almost always want.It has all the information the app container has in addition
+        to data specific to the current request.
+        """
+        return current_request.get().state.aspy_service_scope
+
+    def _get_parameters_to_inject(
+        self,
+        target: Callable[..., Any],
+    ) -> dict[str, type]:
+        result: dict[str, type] = {}
+
+        for parameter_name, parameter in inspect.signature(target).parameters.items():
+            if parameter.annotation is Parameter.empty:
+                continue
+
+            injectable_dependency = self._get_injectable_dependency(
+                parameter.annotation.__metadata__
+            )
+
+            if injectable_dependency is None:
+                continue
+
+            service_type = parameter.annotation.__args__[0]
+            result[parameter_name] = service_type
+
+        return result
+
+    def _get_injectable_dependency(
+        self, metadata: Sequence[Any]
+    ) -> InjectableType | None:
+        for metadata_item in metadata:
+            if hasattr(metadata_item, "dependency"):
+                dependency = metadata_item.dependency()  # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType, reportAttributeAccessIssue]
+
+                if isinstance(dependency, InjectableType):
+                    return dependency
+
+        return None
 
 
 class _AspyAsgiMiddleware:
@@ -64,95 +155,3 @@ class _AspyAsgiMiddleware:
                 await self.app(scope, receive, send)
         finally:
             current_request.reset(token)
-
-
-def are_annotated_parameters_with_aspy_dependencies(
-    target: Callable[..., Any],
-) -> bool:
-    for parameter in inspect.signature(target).parameters.values():
-        if parameter.annotation is not None and isinstance(
-            parameter.annotation, InjectableType
-        ):
-            return True
-
-    return False
-
-
-def _inject_routes(routes: list[BaseRoute]) -> None:
-    for route in routes:
-        if not (
-            isinstance(route, APIRoute)
-            and route.dependant.call is not None
-            and not are_annotated_parameters_with_aspy_dependencies(
-                route.dependant.call
-            )
-        ):
-            continue
-
-        route.dependant.call = inject_from_container(route.dependant.call)
-
-
-def inject_from_container(target: Callable[..., Any]) -> Callable[..., Any]:
-    @functools.wraps(target)
-    async def _inject_async_target(*args: Any, **kwargs: Any) -> Any:  # noqa: ANN401
-        parameters_to_inject = get_parameters_to_inject(target)
-        parameters_to_inject_resolved: dict[str, Any] = {
-            injected_parameter_name: await get_request_container().get_service(
-                injected_parameter_class
-            )
-            for injected_parameter_name, injected_parameter_class in parameters_to_inject.items()
-        }
-        return await target(*args, **{**kwargs, **parameters_to_inject_resolved})
-
-    return _inject_async_target
-
-
-def get_request_container() -> ServiceScope:
-    """When inside a request, returns the scoped container instance handling the current request.
-
-    This is what you almost always want.It has all the information the app container has in addition
-    to data specific to the current request.
-    """
-    return current_request.get().state.aspy_service_scope
-
-
-def get_parameters_to_inject(
-    target: Callable[..., Any],
-) -> dict[str, type]:
-    result: dict[str, type] = {}
-
-    for parameter_name, parameter in inspect.signature(target).parameters.items():
-        if parameter.annotation is Parameter.empty:
-            continue
-
-        injectable_dependency = get_injectable_dependency(
-            parameter.annotation.__metadata__
-        )
-
-        if injectable_dependency is None:
-            continue
-
-        service_type = parameter.annotation.__args__[0]
-        result[parameter_name] = service_type
-
-    return result
-
-
-def get_injectable_dependency(metadata: Sequence[Any]) -> InjectableType | None:
-    for metadata_item in metadata:
-        if hasattr(metadata_item, "dependency"):
-            dependency = metadata_item.dependency()  # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType, reportAttributeAccessIssue]
-
-            if isinstance(dependency, InjectableType):
-                return dependency
-
-    return None
-
-
-class FastApiDependencyInjection:
-    def setup(self, app: FastAPI, services: ServiceCollection) -> None:
-        service_provider = services.build_service_provider()
-        app.state.aspy_service_provider = service_provider
-        app.add_middleware(_AspyAsgiMiddleware)
-        _update_lifespan(app, service_provider)
-        _inject_routes(app.routes)
